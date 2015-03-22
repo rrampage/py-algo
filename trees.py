c3efa13945e33a4d3b800a077946719ef63066c3
rrampage <raunak1001@gmail.com>
2015-03-23 01:23:16 +0530

    Added class for Binary search tree node

--- trees.py
+++ trees.py
@@ -6,12 +6,13 @@ class BstNode:
     left = None
     right = None
     parent = None
+    key = None
 
-    # A Binary Tree Node has 3 pointers, 2 to it's children and 1 to it's parent
-    def __init__(self, parent, left, right):
+    # A Binary Tree Node has 3 pointers, 2 to it's children and 1 to it's parent. It stores a key.
+    # When initializing a node, we point it to it's parent and fill it with the relevant data
+    def __init__(self, parent, key):
         self.parent = parent
-        self.left = left
-        self.right = right
+        self.key = key
 
 
 
