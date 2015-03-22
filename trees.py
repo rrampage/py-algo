1f4ee42d034d05d01e837b23b1b2eba423291cd4
rrampage <raunak1001@gmail.com>
2015-03-23 01:42:20 +0530

    Added search, min and max methods. Removed unwanted parameter from __init__

--- trees.py
+++ trees.py
@@ -3,16 +3,50 @@ __author__ = 'rrampage'
 
 
 class BstNode:
+    # A Binary Tree Node has 3 pointers, 2 to it's children and 1 to it's parent. It stores a key.
     left = None
     right = None
     parent = None
     key = None
 
-    # A Binary Tree Node has 3 pointers, 2 to it's children and 1 to it's parent. It stores a key.
-    # When initializing a node, we point it to it's parent and fill it with the relevant data
-    def __init__(self, parent, key):
-        self.parent = parent
+    def __init__(self, key):
         self.key = key
 
+    def insert(self, child):
+        if child.key > self.key:
+            self.right = child
+            child.parent = self
+        else:
+            self.left = child
+            child.parent = self
 
+    @staticmethod
+    def is_root(x):
+        if x.parent is None:
+            return True
+        else:
+            return False
+
+
+def max_node(root):
+    n = root
+    while n.right is not None:
+        n = n.right
+    return n
+
+
+def min_node(root):
+    n = root
+    while n.left is not None:
+        n = n.left
+    return n
+
+
+def search(root, k):
+    if k == root.key:
+        return root
+    elif k < root.key:
+        return search(root.left, k)
+    else:
+        return search(root.right, k)
 
